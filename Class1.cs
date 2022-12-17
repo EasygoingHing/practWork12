ba284ccf6cd3ca50a7f3e92d9f42f9344dadd23b
Мой компьютер <krutolapov98@gmail.com>
2022-12-17 09:06:04 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Крутолапов_пр12
+{
+    internal class Class1
+    {
+        /// <summary>
+        /// Вычисляет площадь и периметр квадрата
+        /// </summary>
+        /// <param name="side">Сторона</param>
+        /// <param name="square">Площадь</param>
+        /// <param name="perimetr">Периметр</param>
+        static public string SquareAndPerimetr(int side)
+        {
+            return $"Площадь - {side * side}\nПериметр - {side * 4}";
+        }
+
+        /// <summary>
+        /// Меняет местами единицы и десятки, откидывая сотни
+        /// </summary>
+        /// <param name="number">Целое число</param>
+        /// <returns></returns>
+        static public string ReplacingNumbers(int number)
+        {
+            return $"Единицы числа - {number % 10}\nДесятки - {number % 100 / 10}";
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using Крутолапов_пр12;
+
+namespace WpfApp12
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonСonclusion_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(tbNumber.Text, out int value) && value.ToString().Length == 3)
+            {
+                tbNumber.Clear();
+                tbNumber.Focus();
+                tbResult1.Text = Class1.ReplacingNumbers(value);
+            }
+            else
+            {
+                MessageBox.Show("Данные введены некорректно или отсутствуют");
+            }
+        }
+
+        private void ButtonSP_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(tbA.Text, out int side) && side > 0)
+            {
+                tbA.Clear();
+                tbA.Focus();
+                tbResult.Text = Class1.SquareAndPerimetr(side);
+            }
+            else
+            {
+                MessageBox.Show("Данные введены некорректно или отсутствуют");
+            }
+        }
+
+        DispatcherTimer timer;//описываем таймер
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            timer.IsEnabled = true;            
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime d = DateTime.Now;
+            time.Text = d.ToString("HH:mm");
+            data.Text = d.ToString("dd.MM.yyyy");
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил Крутолапов Валерий ИСП-31");
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
